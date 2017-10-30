de5437c08592412b1fcbcd262ddef2a90c791fee
fefaleksey <fefaleksey@mail.ru>
2017-10-30 22:01:16 +0300

    Added test project

---
+++ Test/Tests.cs
@@ -0,0 +1,22 @@
+﻿using Xunit;
+using Localization;
+
+namespace Test
+{
+	public class TestsRobot
+	{
+		[Fact]
+		public void TestGetSensorsValue()
+		{
+			var robot = new Robot();
+			var map = new Map();
+			robot.InitialDirection = 3;
+			robot.RSensors.Read(0, 0, 1, robot, map);
+			var value = robot.RSensors.GetSensorsValue(robot);
+			Assert.True(value == 36896);
+			robot.RSensors.Read(0, 6, 1, robot, map);
+			value = robot.RSensors.GetSensorsValue(robot);
+			Assert.True(value == 61440);
+		}
+	}
+}

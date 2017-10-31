c7aa62366bb247a66689f526ded914ac583fb736
fefaleksey <fefaleksey@mail.ru>
2017-10-31 23:32:27 +0300

    Added PrintResult

--- Localization/Generate.cs
+++ Localization/Generate.cs
@@ -23,7 +23,7 @@ namespace Localization
 			var counter = 0;
 			for (var i = 0; i < finalWays.Ways.Count; i++)
 			{
-				while (finalWays.Ways[i][counter]!=8888888)
+				while (finalWays.Ways[i][counter] != 8888888)
 				{
 					counter++;
 				}
@@ -32,10 +32,11 @@ namespace Localization
 			}
 			return max;
 		}
-		
+
 		public int[,] GenerateHashtable(FinalWays finalWays)
 		{
-			var directions = new int[(int) Math.Pow(2, Math.Pow(2,Robot.RobotSensors.QualitySensors)), HashtableLength(finalWays)];
+			var directions = new int[(int) Math.Pow(2, Math.Pow(2, Robot.RobotSensors.QualitySensors)),
+				HashtableLength(finalWays)];
 			var robot = new Robot();
 			var motion = new Motion();
 			var map = new Map();
@@ -45,10 +46,10 @@ namespace Localization
 				var x = finalWays.Ways[i][0];
 				var y = finalWays.Ways[i][1];
 				var initialdirection = finalWays.Ways[i][3];
-				if(initialdirection==8888888) continue;
+				if (initialdirection == 8888888) continue;
 				var newDir = finalWays.Ways[i][2];
 				robot.InitialDirection = 3;
-				robot.RSensors.Read(x, y,newDir /*direction*/, robot, map);
+				robot.RSensors.Read(x, y, newDir /*direction*/, robot, map);
 				var step = 0;
 				var fl = true;
 				//var newDir = direction;
@@ -56,7 +57,7 @@ namespace Localization
 				robot.InitialDirection = initialdirection;
 				//initialdirection = newDir;
 				for (var j = 3; j < finalWays.Ways[i].Count && finalWays.Ways[i][j] != 8888888; j++)
-				{	
+				{
 					var value = robot.RSensors.GetSensorsValue(robot);
 					directions[value, step] = finalWays.Ways[i][j];
 					newDir = motion.GetNewDir(newDir, finalWays.Ways[i][j], beginWay);
@@ -107,7 +108,26 @@ namespace Localization
 					}
 				}
 			}
+			PrintResult(directions);
 			return directions;
+		}
+
+		public void PrintResult(int[,] directions)
+		{
+			int test = 0, test1 = 0;
+			for (var i = 0; i < (int) Math.Pow(2, Math.Pow(2, Robot.RobotSensors.QualitySensors)); i++)
+			{
+				for (var j = 0; j < 8; j++)
+				{
+					test++;
+					if (directions[i, j] != 0)
+					{
+						test1++;
+						Console.WriteLine("key = " + i + " step = " + j + " value = " + directions[i, j]);
+					}
+				}
+			}
+			Console.WriteLine("test = " + test + " test1 = " + test1);
 		}
 	}
 }
